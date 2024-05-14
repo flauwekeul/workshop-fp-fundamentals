e97594fd5f3ed5ff134312f91d7a84bd7ac94a4b
Abbe Keultjes <flauwekeul@gmail.com>
2024-05-14 10:19:15 +0200

    WIP: add yahtzee folder

---
+++ yahtzee/calculations.js
@@ -0,0 +1,74 @@
+import { map, pluck } from 'ramda';
+import {
+  BONUS_SCORE,
+  BONUS_THRESHOLD,
+  FULL_HOUSE_SCORE,
+  LARGE_STRAIGHT_SCORE,
+  MAX_THROWS,
+  NO_SCORE,
+  SMALL_STRAIGHT_SCORE,
+  YAHTZEE_SCORE,
+} from './data.js';
+
+/**
+ * State
+ */
+
+export const decrementThrowsLeft = (state) => state;
+
+export const updateHeldDie = (dieIndex, isHeld, state) => state;
+
+export const updatePlayerScore = (scoreId, score, state) => state;
+
+const otherPlayer = (currentPlayer) => 'player1';
+
+const resetThrowsLeft = (state) => state;
+
+const resetHeldDice = (state) => state;
+
+export const switchPlayer = (state) => state;
+
+export const currentPlayerScores = (state) => state.player1.scores;
+
+export const canHoldDie = (state) => true;
+
+/**
+ * Scores
+ */
+
+// Returns an array of the counts of each kind of die value, e.g.: [2, 2, 3, 5, 5] returns [2, 1, 2]
+const kindsCount = (diceValues) => [];
+
+// Very difficult to come up with this 🤯
+export const sequenceCount = (diceValues) => 0;
+
+// Each calculator is called with a sorted array of dice values, e.g.: [2, 2, 3, 4, 6]
+const scoreCalculators = {
+  ones: (diceValues) => 0,
+  twos: (diceValues) => 0,
+  threes: (diceValues) => 0,
+  fours: (diceValues) => 0,
+  fives: (diceValues) => 0,
+  sixes: (diceValues) => 0,
+  threeOfAKind: (diceValues) => 0,
+  fourOfAKind: (diceValues) => 0,
+  fullHouse: (diceValues) => 0,
+  smallStraight: (diceValues) => 0,
+  largeStraight: (diceValues) => 0,
+  chance: (diceValues) => 0,
+  // When yahtzee is scored when it's been scored before, 100 points should be awarded.
+  // But to keep things simple, yahtzee always scores 50 points.
+  yahtzee: (diceValues) => 0,
+};
+
+export const possibleScores = (dice) => map((calculator) => calculator(pluck('value', dice)), scoreCalculators);
+
+export const upperSectionSum = (scores) => 0;
+
+export const bonus = (sum) => 0;
+
+export const totalScore = (scores) => 0;
+
+export const anyScoresEmpty = (state) => true;
+
+export const winner = 'player1';
---
+++ yahtzee/lib/render.js
@@ -0,0 +1,112 @@
+/* eslint-disable functional/no-expression-statements */
+/* eslint-disable functional/no-return-void */
+
+import {
+  addIndex,
+  curry,
+  filter,
+  flow,
+  forEach,
+  forEachObjIndexed,
+  isNil,
+  join,
+  keys,
+  map,
+  path,
+  prop,
+  reject,
+  tap,
+  toPairs,
+} from 'ramda';
+import {
+  bonus,
+  canHoldDie,
+  currentPlayerScores,
+  possibleScores,
+  totalScore,
+  upperSectionSum,
+  winner,
+} from '../calculations.js';
+import { queryElement, removeAttribute, setElementHtml, toggleAttribute } from './dom.js';
+
+// ⛔️ No need to make changes in this file
+
+const mapWithIndex = addIndex(map);
+
+export const renderCurrentPlayerName = (state) =>
+  flow(state, [path([state.currentPlayer, 'name']), setElementHtml('#current-player')]);
+
+const dieTemplate = curry(
+  (canHold, die, index) => `
+  <li>
+    <label>
+      <input type="checkbox" name="${index}" ${die.hold ? 'checked' : ''} ${canHold ? '' : 'disabled'} />
+      ${die.value}
+    </label>
+  </li>
+`,
+);
+
+export const renderDice = (state) =>
+  flow(state, [prop('dice'), mapWithIndex(dieTemplate(canHoldDie(state))), join(''), setElementHtml('#dice')]);
+
+export const renderTableHeader = (state) => {
+  setElementHtml('#player1-name', state.player1.name);
+  setElementHtml('#player2-name', state.player2.name);
+};
+
+const renderDerivedScores = (state) =>
+  flow(state, [
+    currentPlayerScores,
+    upperSectionSum,
+    tap(setElementHtml(`#${state.currentPlayer}-sum`)),
+    bonus,
+    setElementHtml(`#${state.currentPlayer}-bonus`),
+  ]);
+
+const renderScores = (state) =>
+  flow(state, [
+    currentPlayerScores,
+    reject(isNil),
+    forEachObjIndexed((score, scoreId) => setElementHtml(`#${state.currentPlayer}-${scoreId}`, score)),
+  ]);
+
+export const renderAllScores = (state) => flow(state, [tap(renderDerivedScores), tap(renderScores)]);
+
+export const renderPossibleScores = (state) => {
+  flow(possibleScores(state.dice), [
+    toPairs,
+    map(([scoreId, possibleScore]) => ({ score: state[state.currentPlayer].scores[scoreId], possibleScore, scoreId })),
+    filter(({ score }) => isNil(score)),
+    forEach(({ possibleScore, scoreId }) => {
+      setElementHtml(
+        `#${state.currentPlayer}-${scoreId}`,
+        `<button data-score-id="${scoreId}" data-score="${possibleScore}">${possibleScore}</button>`,
+      );
+    }),
+  ]);
+};
+
+export const clearPossibleScores = (state) =>
+  flow(state, [
+    path([state.currentPlayer, 'scores']),
+    filter(isNil),
+    keys,
+    forEach((scoreId) => setElementHtml(`#${state.currentPlayer}-${scoreId}`, '')),
+  ]);
+
+export const renderThrowsLeft = (state) => {
+  setElementHtml('#throws-left', state.throwsLeft);
+  toggleAttribute('disabled', state.throwsLeft === 0, queryElement('#roll-dice'));
+};
+
+export const renderTotalScore = (state) => {
+  setElementHtml('#player1-total', totalScore(state.player1.scores));
+  setElementHtml('#player2-total', totalScore(state.player2.scores));
+};
+
+export const renderWinner = (state) => {
+  const player = winner(state);
+  setElementHtml('#winner-name', player.name);
+  removeAttribute('hidden', queryElement('#winner'));
+};
